1acfe8e4532d39468b8995beb29012d1a459ffbc
Arka Datta <77283508+canbethegodfatherofyourlife@users.noreply.github.com>
2022-08-19 06:59:58 +0530

    Create default.sol

---
+++ default.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense 
+
+pragma solidity <= 0.8.9 >=0.8.0; 
+
+contract Default{
+
+    uint public a;
+    int public b;
+    address public c;  // followed by 40 zeroes
+    bool public d;  
+    string public e;
+}
