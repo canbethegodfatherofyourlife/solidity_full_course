2e239a3043ee49bc64f58395d115a6d0700e5e62
Arka Datta <77283508+canbethegodfatherofyourlife@users.noreply.github.com>
2022-08-19 07:00:53 +0530

    Create func.sol

---
+++ func.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense 
+
+pragma solidity <= 0.8.9 >=0.8.0;   
+
+contract Name{
+
+    function functionName(parameter _dtype parameter) visibility type returns(dtype){
+        .....
+        // type -> view / pure 
+        // visibility -> internal/ external/ public/ private 
+    }
+
+    
+}
