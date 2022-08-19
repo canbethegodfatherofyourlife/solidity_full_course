ec16cd8c74490d81c9c3ebe3c0e31d775157f5ec
Arka Datta <77283508+canbethegodfatherofyourlife@users.noreply.github.com>
2022-08-19 06:57:39 +0530

    Create integers.sol

---
+++ integers.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense 
+
+pragma solidity <= 0.8.9 >=0.8.0; 
+
+// int -> +ve and -ve 
+// uint -> only +ve 
+
+// uint == uint256 
+// int8 -> -128 to 127 
+// uint8 -> 0 to 255 
+
+// intn -> -2^(n-1) to 2^(n-1)-1 
+// uintn -> 0 to 2^(n)-1 
+
