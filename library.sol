956e802dea60f3e9f4e315a8e86880409f900a36
Arka Datta <77283508+canbethegodfatherofyourlife@users.noreply.github.com>
2022-08-19 07:15:05 +0530

    Create library.sol

---
+++ library.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity <=0.8.9 >=0.8.0;  
+
+// functions should be internal 
+
+library SafeMath {
+    function add(uint x, uint y) internal pure returns (uint) {
+        uint z = x + y;
+        require(z >= x, "uint overflow");
+
+        return z;
+    }
+}
+
+library Array {
+    function remove(uint[] storage arr, uint index) public {
+        // Move the last element into the place to delete
+        require(arr.length > 0, "Can't remove from empty array");
+        arr[index] = arr[arr.length - 1];
+        arr.pop();
+    }
+}
+
+// 1 2 3 
